a216ef136f71fe74f612cd408bce2fd932ec53ae
Brody Spearman <brodyspearman@gmail.com>
2026-02-27 16:09:12 -0600

    Update route.ts
    Fixed uuid persistence issues.

--- app/api/session/route.ts
+++ app/api/session/route.ts
@@ -1,7 +1,14 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
 // Proxy function for session management
-export async function GET() {
+export async function GET(request: NextRequest) {
+    const existingSession = request.cookies.get('session_id');
+
+    if (existingSession) {
+        console.log("Existing session found: ", existingSession.value);
+        return NextResponse.json({ sessionId: existingSession.value });
+    }
+
     const baseUrl = process.env.NODE_ENV === 'development'
         ? 'http://localhost:8000'
         : 'https://buffshortener.vercel.app';
