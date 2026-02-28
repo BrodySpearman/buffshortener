74371b87f38fa009805a2ebffa468b70a46522f5
Brody Spearman <brodyspearman@gmail.com>
2026-02-27 18:44:55 -0600

    Route documentation refactoring

--- api/routes.py
+++ api/routes.py
@@ -13,17 +13,18 @@ from api.features.Session.getSession import get_session
 # http://localhost:8000/api/docs -- API documentation
 # Database models are stored in api/db/models/model.py
 
-# Redirect from short URL to Long URL
+### Routes ###
+
+"""
+    Redirects from short URL to Long URL.
+    URLRedirect: object
+    {
+        "shortUrl": "buff.st/123456",
+        "longUrl": "https://www.youtube.com"
+    }
+"""
 @app.get("/buff.st/{shortUrl}")
 async def redirect_url(shortUrl: str):
-    """
-        Redirects from short URL to Long URL.
-        URLRedirect: object
-        {
-            "shortUrl": "buff.st/123456",
-            "longUrl": "https://www.youtube.com"
-        }
-    """
     url = await app.collection.find_one({ "shortUrl": f"buff.st/{shortUrl}" })
     if url:
         print("Redirecting to: ")
@@ -31,15 +32,15 @@ async def redirect_url(shortUrl: str):
         return RedirectResponse(url["longUrl"])
     return {'message': 'URL not found'}
 
+"""
+    Creates a new anonymous (non-logged in) session.
+    ANONYMOUS_SESSION: object
+    {
+        "sessionId": "123456"
+    }
+"""
 @app.get("/api/sessions/anonymous")
 async def create_anonymous_session(response: Response):
-    """
-        Creates a new anonymous (non-logged in) session.
-        ANONYMOUS_SESSION: object
-        {
-            "sessionId": "123456"
-        }
-    """
     session_id = str(uuid.uuid4())
 
     response.set_cookie(
@@ -54,33 +55,33 @@ async def create_anonymous_session(response: Response):
     print(f'New session created: {session_id}')
     return {'sessionId': session_id}
 
+"""
+    Fetches all user (or anon session) input URLs and corrosponding aliases.
+    URL_LIST: object[]
+    {
+        "inputUrl": "https://www.youtube.com",
+        "shortUrl": "buff.st/123456"
+    }
+"""
 @app.get("/api/show-url-list")
 async def show_url_list(session_id: str = Depends(get_session)):
-    """
-        Fetches all past input URLs and corrosponding aliases.
-        URL_LIST: object[]
-        {
-            "inputUrl": "https://www.youtube.com",
-            "shortUrl": "buff.st/123456"
-        }
-    """
+
     url_list = []
     async for url in app.collection.find({"owner.session_id": session_id}).limit(10): 
         url_list.append(URLListRecord(inputUrl=url['longUrl'], shortUrl=url['shortUrl']))
 
     print("URL List Received")
     return url_list
-   
 
+"""
+    Submits a new URL to the database.
+    URLPost: object
+    {
+        "inputUrl": "https://www.youtube.com"
+    }
+"""
 @app.post("/api/submit-url")
 async def submit_url(url: URLPost, session_id: str = Depends(get_session)):
-    """
-        Submits a new URL to the database.
-        URLPost: object
-        {
-            "inputUrl": "https://www.youtube.com"
-        }
-    """
     inputUrl = url.inputUrl
     
     new_db_url = db_url(
@@ -104,14 +105,14 @@ async def submit_url(url: URLPost, session_id: str = Depends(get_session)):
 
     return {'message': 'URL submitted successfully', 'shortUrl': new_db_url.shortUrl}
 
+"""
+    Deletes a URL from the database.
+    URLPost: object
+    {
+        "shortUrl": "buff.st/123456"
+    }
+"""
 @app.delete("/api/delete-url")
 async def delete_url(url: URLDelete):
-    """
-        Deletes a URL from the database.
-        URLPost: object
-        {
-            "shortUrl": "buff.st/123456"
-        }
-    """
     await app.collection.delete_one({ "shortUrl": url.shortUrl })
     return {'message': 'URL deleted successfully'}
